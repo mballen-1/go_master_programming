37d224783060846ab135fcc63754b56afd826977
Miguel Ballen <mballen@mcclatchyservices.com>
2021-05-18 09:32:32 -0500

    sections 1-3

---
+++ application_structure/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+const secondsInAnHour = 3600
+
+func main() {
+	fmt.Println("Hello world!")
+	distance := 60.8
+	fmt.Printf("The distance in miles is  %f", distance*0.62137)
+}
