558424cb3ee7caa87bb72ac5e4305edb809965e5
Miguel Ballén <mballeng91@gmail.com>
2021-06-10 18:58:38 -0500

    [go_master] -Add exercises sections 2, 5 and 7

---
+++ constants/main.go
@@ -0,0 +1,97 @@
+/*package main*/
+
+/*func main(){
+	const days int = 7
+	var myStr1 = fmt.Sprintf("%.3f", 234.5)
+	fmt.Println(myStr1)
+}*/
+
+/*func main(){
+	const (
+		daysWeek = 7
+		lightSpeed = 299792458
+		pi = 3.14159
+		secPerDay = 3600 * 24
+		daysYear = 365
+		)
+	fmt.Println(secPerDay * daysYear)
+}*/
+
+/*func main() {
+	const x int = 10
+
+	// declaring a constant of type slice int ([]int)
+	var m = []int{1: 3, 4: 5, 6: 8}
+	_ = m
+}*/
+
+package main
+
+/*import "math"
+
+func main() {
+	const a int = 7
+	const b float64 = 5.6
+	const c = float64(a) * b
+
+	x := 8
+	var xc int = x
+	_ = xc
+	var noIPv6 = math.Pow(2, 128)
+	_ = noIPv6
+}*/
+
+/*func main(){
+/*const (
+	Jun = iota + 6
+	Jul
+	Ago
+)
+fmt.Println(Jun, Jul, Ago)*/
+/*x, y, z := 10, 15.5, "Gophers"
+score := []int{10, 20, 30}*/
+// fmt.Printf("%d, %.1f, %s, %v", x, y, z, score)
+
+// fmt.Printf("%v, %v, %v, %v", x, y, z, score)
+
+/*fmt.Printf("%T, %T, %T, %T", x, y, z, score)
+}*/
+
+func main() {
+	
+}
+
+// Section 5: Go - basics.
+
+// Package fmt
+
+// 1 -> https://play.golang.org/p/ZHX5mBLBBIl
+// 2. -> https://play.golang.org/p/yoQn1ozs6tA
+// 3 -> https://play.golang.org/p/DtOnYhZPSPR
+
+// Operators and conversions
+// 1. -> https://play.golang.org/p/sBlwulU6AUd
+// 2. -> https://play.golang.org/p/RJb1fov2WIp
+// 3. -> https://play.golang.org/p/KrzzX57LpXr
+// 4. -> https://play.golang.org/p/WCDKDk0OjEf
+// 5. -> https://play.golang.org/p/PxoNyjT2xYr
+
+// Named Types and Aliases
+
+// 1. -> https://play.golang.org/p/Z-mgkjnwz62
+// 2. -> https://play.golang.org/p/bUROu1D62-u
+// 3. -> https://play.golang.org/p/tSFGo7KI37o
+
+
+// Section 7: Flow control
+
+// 1. -> https://play.golang.org/p/wR9rrjAv4Yo
+// 2. -> https://play.golang.org/p/s7lJLjOJAC4
+// 3. -> https://play.golang.org/p/plwi3c-AyBn
+// 4. -> https://play.golang.org/p/HjtHegeAIRz
+// 5. -> https://play.golang.org/p/Z8jeJNyLvU5
+// 6. -> https://play.golang.org/p/7dfhtsTMSI4
+
+
+
+
---
+++ ex2_declare_variables/main.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+/*import "fmt"*/
+
+/*func main() {
+	var a int; var b float64
+	var c bool; var d string
+	x := 20; y := 15.5; z := "Gopher"
+	fmt.Println(a, b, c, d, x, y, z)
+}*/
+
+/*func main() {
+	var (
+		a int
+		b float64
+		c bool
+		d string
+	)
+	x, y, z := 20, 15.5, "Gopher"
+	_, _, _, _, _, _, _ = a, b, c, d, x, y, z
+}*/
+
+/*func main() {
+	var a float64 = 7.1
+
+	x, y := true, 3.7
+
+	a, x = 5.5, false
+
+	_, _, _ = a, x, y
+}*/
+
+func main() {
+	version := "3.1"
+	_ = version
+	name := "Golang"
+	fmt.Println(name)
+}
